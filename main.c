f468679645d924f46f105bdcfbcc153fbd49d9e8
kaseyleavell <47652845+kaseyleavell@users.noreply.github.com>
2019-04-28 15:10:54 -0600

    WORKING
    low sampling frequency

--- main.c
+++ main.c
@@ -5,23 +5,29 @@
  * Final Project main.c
  */
 #define LED 0x04;
-#define SLEEP; __delay_cycles(700);
+#define SLEEP; __delay_cycles(4000);
 
 int sample = 0;
 unsigned volatile int outVal = 0;
 unsigned int adc;
 int fired = 0;
 int i = 0;
+int temp = 0;
+int temp1 = 0;
+int temp2 = 0;
 int startInc = 0;
+
 void readADC();
 void startComm();
 void setLine();
+void stopComm();
 
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-	//ADC10CTL0 = ADC10SHT_2 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
-	// ADC10CTL1 = INCH_0 + CONSEQ2;						// input A1
+
+	ADC10CTL0 = ADC10SHT_2 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
+	ADC10CTL1 = INCH_0;						// input A1
 	ADC10AE0 |= 0x01;                         // PA.1 ADC option select
 	P1OUT &=~ 0xFF;
 	P1DIR &=~ 0x03;
@@ -30,9 +36,7 @@ int main(void)
 
 	//beginning comm timer
 
-	CCR0 = 50000;
-
-	outVal = 1023;
+	CCR0 = 200;
 	//pwm timer
 	//outVal = 726;
 	P2DIR |= LED;
@@ -44,18 +48,17 @@ int main(void)
 	TA1CTL = TASSEL_2 + MC_1;
 	TACTL = TASSEL_2 + MC_1;
 	__enable_interrupt();
-	//ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
+	ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
 	//CCTL0 = CCIE;
-	startComm();
 	while(1)
 	{
-		/*
-    	if(sample == 1)
-    	{
-    		startComm();
-    		sample = 0;
-    	}
-		 */
+		if(sample == 1)
+		{
+			sample = 0;
+			startComm();
+
+		}
+
 	}
 }
 // ADC10 interrupt service routine
@@ -67,9 +70,9 @@ __interrupt void ADC10_ISR(void)
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void Timer_A (void)
 {
-	if(i<10)
+	if(i<11)
 	{
-		if(outVal & 0x0200)
+		if(outVal & 0x0400)
 		{
 			//turn on P2DIR
 			TA1CCR1 = 13;
@@ -80,37 +83,48 @@ __interrupt void Timer_A (void)
 		i++;
 		outVal = outVal << 1;
 	}
-	if(i==10)
+	if(i==11)
 	{
 		i = 0;
 		//outVal = 0;
 		//testing multiple byte sending
-		outVal = 682;
+		stopComm();
 	}
 }
 void startComm()
 {
 	//activate comm timer after pulling line high
-	outVal = 682; //bin 1010101010
-	//clock off starting 10-bits
+	outVal |= 0x0400;
+	//clock on starting 10-bits
 	CCTL0 = CCIE;
 }
-void setLine()
+void stopComm()
 {
-	//the value of pushVal corresponds to the PWM to send
-	if(outVal & 0x0200)
-	{
-		//turn on P2DIR
-		TA1CCR1 = 13;
-	}else
-	{
-		TA1CCR1 = 0;
-	}
-	i++;
-	outVal = outVal << 1;
+	CCTL0 &=~ CCIE;
+	SLEEP;
+	TA1CCR1 = 0;
+	outVal = 0;
+	ADC10CTL0 |= ADC10IE;
+	ADC10CTL0 |= ENC + ADC10SC;
+//	if(startInc == 3)
+//	{
+//		SLEEP;
+//	}
 }
 void readADC()
 {
+	ADC10CTL0 &=~ ADC10IFG;
+	ADC10CTL0 &=~ ADC10IE;
 	outVal = ADC10MEM;
+//	if(startInc == 0){
+//		temp = outVal;
+//	}if(startInc == 1)
+//	{
+//		temp1 = outVal;
+//	}if(startInc == 2)
+//	{
+//		temp2 = outVal;
+//	}
 	sample = 1;
+	startInc++;
 }
